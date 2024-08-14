46e95d1da9d8541b63489b1ee8afc3dcfb308e4d
Bartosz Litwa <blitwa@kpmg.pl>
2024-08-14 13:08:18 +0200

    Add initial store

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LanguageService } from '../services/translate.service';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TranslateModule],
+  imports: [RouterOutlet, CommonModule, TranslateModule, HeaderComponent],
   providers: [LanguageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
